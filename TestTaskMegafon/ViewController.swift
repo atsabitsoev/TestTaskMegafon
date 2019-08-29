b8c29726aeab7e9a09c9897d67dc88529ddbe31e
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2019-08-29 16:03:00 +0300

    Initial Commit

---
+++ TestTaskMegafon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskMegafon
+//
+//  Created by Ацамаз Бицоев on 29/08/2019.
+//  Copyright © 2019 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
